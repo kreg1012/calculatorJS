48069137c19447af09d81786c46ad66b6ee18cc0
kreg1012 <kreg1012@gmail.com>
2022-12-06 19:47:38 -0500

    evnt listeners for buttons. Updating display

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-const calcInput;
+const calcInput = document.getElementById('display');
 const buttons = document.querySelectorAll('button');
+
+console.table(buttons);
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        calcInput.textContent += button.textContent; 
+    });
+});
