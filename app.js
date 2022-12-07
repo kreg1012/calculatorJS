45e2fa62d439a1a0ebb9ed90f311a06ebb06af46
kreg1012 <kreg1012@gmail.com>
2022-12-07 16:29:30 -0500

    converted event listener to ternary operator

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const calcInput = document.getElementById('display');
 const buttons = document.querySelectorAll('button');
 
+calcInput.textContent = '0';
+
 console.table(buttons);
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        calcInput.textContent += button.textContent; 
+        calcInput.textContent === '0' ? (calcInput.textContent = '', calcInput.textContent += button.textContent) 
+        : calcInput.textContent += button.textContent;
+        
     });
 });
