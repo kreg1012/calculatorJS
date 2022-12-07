b6055a60581063170aefc17554f7704f6c9c17a0
kreg1012 <kreg1012@gmail.com>
2022-12-07 18:25:35 -0500

    event listener and handler for operator btns

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const calcInput = document.getElementById('display');
-const buttons = document.querySelectorAll('button');
+const numbers = document.querySelectorAll('#numberCont button');
 
 calcInput.textContent = '0';
 
-console.table(buttons);
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        calcInput.textContent === '0' ? (calcInput.textContent = '', calcInput.textContent += button.textContent) 
-        : calcInput.textContent += button.textContent;
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        calcInput.textContent === '0' ? (calcInput.textContent = '', calcInput.textContent += number.textContent) 
+        : calcInput.textContent += number.textContent;
         
     });
 });
